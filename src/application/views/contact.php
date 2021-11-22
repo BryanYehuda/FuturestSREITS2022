05564173e54d3c19b6938bf4d42e303c34015f67
Rama Muhammad <rmurshal@gmail.com>
2021-11-23 00:29:41 +0800

    Add contact page

--- src/application/views/contact.php
+++ src/application/views/contact.php
@@ -11,6 +11,7 @@
     $this->load->view('template/css');
   ?>
   <!-- Import css if need -->
+  <link rel="stylesheet" href="<?php echo base_url() ?>public/css/pages/contact.css">
 </head>
 
 <body>
@@ -21,9 +22,55 @@
   </header>
 
   <main id="section">
-    <div class="container">
-      <h1 class="text-center py-5">Hello World!</h1>
-    </div>
+
+   <section id="jumbotron" class="d-flex align-items-center position-relative">
+      <div class="container text-center">
+         <h1 class="mb-5">LOREM IPSUM</h1>
+         <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem odit assumenda amet totam vitae, vero aspernatur deleniti quae voluptatibus, ut quod soluta omnis recusandae vel quasi aliquam libero ratione dolores doloribus quia praesentium. Ipsum, voluptas!
+         </p>
+      </div>   
+   </section>
+
+   <section id="sosmed" class="d-flex align-items-center">
+      <div class="container h-100 d-flex flex-column justify-content-center">
+         <h2 class="text-center mb-2 mb-md-5">Search us on social media!</h2>
+         <div class="row" style="height: 40%;">
+            <a href="" class="card-sosmed col m-2 mx-md-3 mx-lg-5 d-flex flex-column align-items-center justify-content-center">
+               <i class="bi bi-linkedin"></i>
+               <h3>350+</h3>
+               <h4 class="text-center">LinkedIn Connection</h4>
+            </a>
+            <a href="" class="card-sosmed col m-2 mx-md-3 mx-lg-5 d-flex flex-column align-items-center justify-content-center">
+               <i class="bi bi-instagram"></i>
+               <h3>2500+</h3>
+               <h4 class="text-center">Instagram Followers</h4>
+            </a>
+            <a href="" class="card-sosmed col m-2 mx-md-3 mx-lg-5 d-flex flex-column align-items-center justify-content-center">
+               <i class="bi bi-envelope"></i>
+               <h4 class="text-center">Email</h4>
+            </a>
+         </div>
+      </div>
+   </section>
+
+   <section id="contact-us" class="d-flex align-items-center">
+      <div class="container h-75 text-white">
+         <h2 class="text-center mb-5">Message Us!</h2>
+         <form action="">
+            <div class="mb-5">
+               <label for="exampleFormControlInput1" class="form-label">Email address</label>
+               <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter your email here...">
+            </div>
+            <div class="mb-5">
+               <label for="exampleFormControlTextarea1" class="form-label">Message Text</label>
+               <textarea class="form-control" id="exampleFormControlTextarea1" rows="5" placeholder="Enter a message here..."></textarea>
+            </div>
+            <button class="btn btn-lg d-block m-auto">Submit</button>
+         </form>
+      </div>
+   </section>
+   
   </main>
 
   <footer>
