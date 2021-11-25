e32d87dedf2caf745318b73c70b03b8e9bd7a0a2
Bryan Yehuda <bryanyehuda@gmail.com>
2021-11-26 01:14:12 +0700

    Adding Timeline to Detail page

--- src/application/views/details.php
+++ src/application/views/details.php
@@ -12,6 +12,7 @@
   ?>
   <!-- Import css if need -->
   <link rel="stylesheet" href="<?php echo base_url() ?>public/css/pages/details.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
@@ -88,6 +89,58 @@
         </div>
       </div>
     </div>   
+  </section><br><br><br>
+
+  <section id="timeline" class="d-flex align-items-center position-relative">
+    <div class="container">                      
+      <div class="row text-center justify-content-center mb-5">
+        <div class="col-xl-6 col-lg-8">
+          <h2 class="font-weight-bold">Our Evolution</h2>
+          <p class="text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <div class="timeline-steps aos-init aos-animate" data-aos="fade-up">
+            <div class="timeline-step">
+              <div class="timeline-content" data-toggle="popover" data-trigger="hover" data-placement="top" title="" data-content="And here's some amazing content. It's very engaging. Right?" data-original-title="2003">
+                <div class="inner-circle"></div>
+                  <p class="h6 mt-3 mb-1">2003</p>
+                  <p class="h6 text-muted mb-0 mb-lg-0">Lorem ipsum dolor</p>
+                </div>
+              </div>
+              <div class="timeline-step">
+                <div class="timeline-content" data-toggle="popover" data-trigger="hover" data-placement="top" title="" data-content="And here's some amazing content. It's very engaging. Right?" data-original-title="2004">
+                  <div class="inner-circle"></div>
+                  <p class="h6 mt-3 mb-1">2004</p>
+                  <p class="h6 text-muted mb-0 mb-lg-0">Lorem ipsum dolor</p>
+                </div>
+              </div>
+              <div class="timeline-step">
+                <div class="timeline-content" data-toggle="popover" data-trigger="hover" data-placement="top" title="" data-content="And here's some amazing content. It's very engaging. Right?" data-original-title="2005">
+                  <div class="inner-circle"></div>
+                  <p class="h6 mt-3 mb-1">2005</p>
+                  <p class="h6 text-muted mb-0 mb-lg-0">Lorem ipsum dolor</p>
+                </div>
+              </div>
+              <div class="timeline-step">
+                <div class="timeline-content" data-toggle="popover" data-trigger="hover" data-placement="top" title="" data-content="And here's some amazing content. It's very engaging. Right?" data-original-title="2010">
+                  <div class="inner-circle"></div>
+                  <p class="h6 mt-3 mb-1">2010</p>
+                  <p class="h6 text-muted mb-0 mb-lg-0">Lorem ipsum dolor</p>
+                </div>
+              </div>
+              <div class="timeline-step mb-0">
+                <div class="timeline-content" data-toggle="popover" data-trigger="hover" data-placement="top" title="" data-content="And here's some amazing content. It's very engaging. Right?" data-original-title="2020">
+                  <div class="inner-circle"></div>
+                  <p class="h6 mt-3 mb-1">2020</p>
+                  <p class="h6 text-muted mb-0 mb-lg-0">Lorem ipsum dolor</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
   </section><br><br><br>
 
   <footer>
