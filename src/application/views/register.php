e03d0238800c3af3d1c3b056b8c359f6e4704a80
danishputradandi <masdanishpd@gmail.com>
2021-11-28 15:59:15 +0700

    register fix

--- src/application/views/register.php
+++ src/application/views/register.php
@@ -2,7 +2,7 @@
 
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="" rel="stylesheet">
+
 
 <html lang="en" dir="ltr">
 
@@ -20,27 +20,24 @@
 
 
 <body>
-   <?php
-      $this->load->view('components/navbar');
-   ?>
 
+  <header>
+    <?php
+    $this->load->view('components/navbar');
+    ?>
+  </header>
   <br><br><br><br><br>
   <main id="section">
     <div class="container">
 
       <div class="container register center">
         <div class="row">
-
           <div class=".col-6 .col-md-10 register-right ">
             <br>
             <h1 class=" register text-center  ">REGISTER HERE<br> </h1>
-
             <div class="register-form mt-3 mb-3">
-
-              <div class="container">
-
-                <div class="row">
-
+              <div class="container" style="padding-right: 0.4rem; padding-left: 0.25rem;">
+                <div class="row" style="padding-right: 0 !important; padding-left: 0 !important;">
                   <div class="col">
                   </div>
 
@@ -74,87 +71,89 @@
 
 
                       </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
+
+
+
+        <div class="register center">
+          <div class="row">
+
+            <div class=".col-6 .col-md-10 register-right ">
+
+              <h2 class="text-center ">INSERT YOUR DATA <br> </h2>
+
+              <div class="register-form mt-6 mb-6">
+
+                <div class="container">
+
+                  <div class="row">
+
+                    <div class="col">
                     </div>
 
-                  </div>
-
-                </div>
-
-              </div>
 
 
+                    <div class="col-md-13  container">
+                      <div class="col-md-7  container">
+                        <form action="#" class="p-5 ">
+                          <div class="form-group text-dark ">
 
-              <div class="register center">
-                <div class="row">
+                            <label for="name" class="text-dark "><i class="text-dark float"> </i> Institution</label>
+                            <input type="text" class="form-control text-dark">
+                          </div><br>
 
-                  <div class=".col-6 .col-md-10 register-right ">
-
-                    <h2 class="text-center ">INSERT YOUR DATA <br> </h2>
-
-                    <div class="register-form mt-6 mb-6">
-
-                      <div class="container">
-
-                        <div class="row">
-
-                          <div class="col">
+                          <div class="form-group">
+                            <label for="exampleFormControlFile1">Image</label><br>
+                            <input type="file" class="form-control-file" id="exampleFormControlFile1">
                           </div>
+                          <hr><br>
 
-
-
-                          <div class="col-md-13  container">
-                            <div class="col-md-7  container">
-                              <form action="#" class="p-5 ">
-                                <div class="form-group text-dark ">
-
-                                  <label for="name" class="text-dark "><i class="text-dark float"> </i> Institution</label>
-                                  <input type="text" class="form-control text-dark">
-                                </div><br>
-
-                                <div class="form-group">
-                                  <label for="exampleFormControlFile1">Image</label><br>
-                                  <input type="file" class="form-control-file" id="exampleFormControlFile1">
-                                </div>
-                                <hr><br>
-
-                                <div class="form-group">
-                                  <label for="exampleFormControlFile1">Identity Card</label><br>
-                                  <input type="file" class="form-control-file" id="exampleFormControlFile1">
-                                </div>
-                                <hr><br>
-
-
-                                <div class="btn-group">
-                                  <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Choose Competition
-                                  </button>
-                                  <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">RE Innovations Brainstorming - Rp. Lorem Ipsum</a></li>
-                                    <li><a class="dropdown-item" href="#">Essay Competition - Rp. Lorem Ipsum</a></li>
-                                  </ul>
-                                </div>
-                                <hr>
-                                <div class="form-group">
-                                  <br>
-                                  <label for="exampleFormControlFile1">Payment</label><br>
-                                  <input type="file" class="form-control-file" id="exampleFormControlFile1">
-                                </div>
-                                <hr><br>
-                                <!-- Example single danger button -->
-
-
-
-                                <button type="submit" class="btn btn-danger mb-1 mt-1 float-center">Register</button>
-                              </form>
-                            </div>
+                          <div class="form-group">
+                            <label for="exampleFormControlFile1">Identity Card</label><br>
+                            <input type="file" class="form-control-file" id="exampleFormControlFile1">
                           </div>
-                        </div>
+                          <hr><br>
+
+
+                          <div class="btn-group">
+                            <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                              Choose Competition
+                            </button>
+                            <ul class="dropdown-menu">
+                              <li><a class="dropdown-item" href="#">RE Innovations Brainstorming - Rp. Lorem Ipsum</a></li>
+                              <li><a class="dropdown-item" href="#">Essay Competition - Rp. Lorem Ipsum</a></li>
+                            </ul>
+                          </div>
+                          <hr>
+                          <div class="form-group">
+                            <br>
+                            <label for="exampleFormControlFile1">Payment</label><br>
+                            <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                          </div>
+                          <hr><br>
+                          <!-- Example single danger button -->
+
+                          <button type="submit" class="btn btn-danger mb-1 mt-1 float-center">Register</button>
+                        </form>
                       </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </main>
-  <div class="row">
 
-  </div>
   <br><br><br><br><br><br>
   <footer>
     <?php
