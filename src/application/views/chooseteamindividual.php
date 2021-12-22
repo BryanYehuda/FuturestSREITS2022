872d956c2c76ecc70e6bf74ee80db5a5f3181b95
danishputradandi <masdanishpd@gmail.com>
2021-12-23 00:30:44 +0700

    register choose team

---
+++ src/application/views/chooseteamindividual.php
@@ -0,0 +1,93 @@
+<!-- <!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+
+<head>
+    <?php
+    $this->load->view('template/meta');
+    ?>
+    <title>Register</title>
+    <?php
+    $this->load->view('template/css');
+    ?>
+    <link rel="stylesheet" href="<?php echo base_url() ?>public/css/pages/register.css">
+    <!-- Import css if need -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+</head>
+
+
+<body>
+
+    <header>
+        <?php
+        $this->load->view('components/navbar');
+        ?>
+    </header>
+
+    <br><br><br><br><br>
+    <main id="section">
+        <div class="container">
+
+            <div class="container register center">
+
+                <!-- Main Data -->
+                <div class="row">
+                    <div class=".col-6 .col-md-10 register-right ">
+                        <br>
+                        <h1 class="register text-center">Essay Competition <b>Individual / Team</b></h1>
+                        <div class="register-form mt-3 mb-3">
+                            <div class="">
+                                <div class="row">
+                                    <div class="col-md-13  container">
+                                        <div class="col-md-7  container">
+                                            <!-- Form -->
+                                            <form action="#" class="">
+
+                                                <label for="pwd"><i class="float"></i> Team / Individual </label>
+                                                <select class="form-select" aria-label="Default select example">
+                                                    <option selected>-</option>
+                                                    <option value="1"> Individual</option>
+                                                    <option value="2">Team</option>
+
+                                                </select><br>
+
+
+                                                <!-- Example single danger button -->
+                                            </form>
+                                            <!-- End Form -->
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Main Data -->
+
+
+
+                <!-- Event Data -->
+
+                <!-- End Event Data -->
+
+            </div>
+        </div>
+    </main>
+
+    <br><br><br><br><br>
+
+    <footer>
+        <?php
+        $this->load->view('components/footerblue');
+        ?>
+    </footer>
+
+    <?php
+    $this->load->view('template/script');
+    ?>
+
+    <!-- Import js if need -->
+</body>
+
+</html> -->
