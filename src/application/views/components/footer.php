25feffc331e288f93be19264045561211aa4e1a5
Bryan Yehuda <bryanyehuda@gmail.com>
2022-02-11 00:11:55 +0700

    Undoing Changes

--- src/application/views/components/footer.php
+++ src/application/views/components/footer.php
@@ -3,10 +3,10 @@
   <div class="container py-5">
 
     <!-- Links -->
-    <div class="row pt-5 mb-sm-5 text-center">
+    <div class="row pt-5 mb-sm-5">
 
       <!-- col 1 -->
-      <div class="col-md-2 d-flex flex-column mb-5 mb-md-1 offset-md-3">
+      <div class="col-md-2 d-flex flex-column mb-5 mb-md-1 ">
         <h3 class="mb-4">Futurest</h3>
         <a href="https://www.instagram.com/futurest2022/" target="_blank" class="mb-3">Instagram</a>
         <a href="https://www.linkedin.com/company/futureenergysummit/" target="_blank" class="mb-3">LinkedIn</a>
@@ -33,7 +33,7 @@
     <br>
 
     <!-- Image -->
-    <img src="<?php echo base_url() ?>/public/img/logo-formal.png" alt="Logo Formal" class="img-fluid d-block mx-auto">
+    <img src="<?php echo base_url() ?>/public/img/logo-formal.png" alt="Logo Formal" class="img-fluid d-block ms-auto">
 
   </div>
 </div>
