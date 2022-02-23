1d1ae86f3b4f78948f544ad8d1aad759775d7f0e
Rama Muhammad Murshal <rmurshal@gmail.com>
2022-02-23 13:39:52 +0700

    add sun logo (sponsor) at home

--- src/application/views/home.php
+++ src/application/views/home.php
@@ -86,6 +86,7 @@
             <img src="<?= base_url() ?>/public/img/pdam_surabaya.png" alt="Logo Sponsor" class="img-fluid m-4" width="150" height="150">
             <img src="<?= base_url() ?>/public/img/hutama_karya.png" alt="Logo Sponsor" class="img-fluid m-4" width="150" height="150">
             <img src="<?= base_url() ?>/public/img/coca_cola.png" alt="Logo Sponsor" class="img-fluid m-4" width="150" height="150">
+            <img src="<?= base_url() ?>/public/img/sun.jpg" alt="Logo Sponsor" class="img-fluid m-4" width="150" height="150">
           </div>
         </div>
       </div>
